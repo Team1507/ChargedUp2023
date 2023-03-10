6fc690b2db932e8e8f5918194823e8a31e83fcff
Warlocks1507Programmer <programmers@warlocks1507.com>
2023-03-10 18:00:23 -0500

    started autos 3 piece

--- src/main/cpp/commands/AutoMoveToCone.cpp
+++ src/main/cpp/commands/AutoMoveToCone.cpp
@@ -8,7 +8,15 @@
 #include "commands/CmdDriveStraight.h"
 #include "commands/CmdDriveStraightGyro.h"
 #include "commands/CmdDriveTurn2Angle.h"
-
+#include "commands/CmdGyroSetAngleOffset.h"
+#include "commands/CmdPouchIntakeDeploy.h"
+#include "commands/CmdPouchIntakeSetPower.h"
+#include "commands/CmdSetRampPosition.h"
+#include "commands/CmdClawIntakeEnable.h"
+#include "commands/CmdWristSetPosition.h"
+#include "commands/CmdClawSetOuttakePower.h"
+#include "commands/CmdClawEject.h"
+#include "commands/CmdPouchIntakeRetract.h"
 AutoMoveToCone::AutoMoveToCone( Drivetrain *drivetrain, Arm *arm, Camera *camera, Claw *claw, Pouch *pouch ) 
 {
   AddCommands
@@ -16,11 +24,31 @@ AutoMoveToCone::AutoMoveToCone( Drivetrain *drivetrain, Arm *arm, Camera *camera
 		  CmdPrintText("Auto AutoMoveToCone"),
       //Initial Setup
       CmdDriveClearAll(drivetrain),
-      frc2::WaitCommand(0.1_s),
+      CmdGyroSetAngleOffset(drivetrain, 180.0),
+      frc2::WaitCommand(1.0_s),
       //-------------------------------
+      CmdWristSetPosition(claw, 3.0),
+      CmdClawSetOuttakePower(claw, -.8),
+      CmdSetRampPosition(pouch, true),
+      CmdClawEject(claw),
+      CmdSetRampPosition(pouch, false),
+      //CmdDriveStraightGyro(drivetrain, .4, 0, 0, 10, false, false, 0.0),
+      CmdDriveStraightGyro(drivetrain, .6, 0, 0, 135, false, false, 0.0),
+      CmdDriveTurn2Angle(drivetrain, .3, -180),
+      CmdPouchIntakeDeploy(pouch),
+      CmdPouchIntakeSetPower(pouch, Pouch::WhatIntake::Outer, .5),
+      CmdSetRampPosition(pouch, true),
+      CmdClawIntakeEnable(claw, true),
+      CmdWristSetPosition(claw, 10),
+      CmdDriveStraightGyro(drivetrain, .2, 90, -180, 3, false, true, 0.0),
+      frc2::WaitCommand(1.0_s),
+      CmdDriveStraightGyro(drivetrain, .2, 0, -180, 30, false, false, 0.0),
+      CmdPouchIntakeRetract(pouch),
+      CmdPouchIntakeSetPower(pouch, Pouch::WhatIntake::Outer, 0),
+      CmdSetRampPosition(pouch, false),
+      CmdDriveTurn2Angle(drivetrain, .3, -175),
+      CmdDriveStraightGyro(drivetrain, .6, -175, 0, 100, false, false, 0.0),
 
-      CmdDriveStraightGyro(drivetrain, .2, 0, 0, 224.0, false, true, 0.0),
-      CmdDriveStraightGyro(drivetrain, .2, 180, 0, 224.0, false, true, 0.0),
 
 
       //-------------------------------
